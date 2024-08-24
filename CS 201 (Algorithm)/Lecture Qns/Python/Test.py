cc74f0fd86cb04c707e4a6e0e415124b57bc333b
Harshal_Sanas <harshalsanas34@gmail.com>
2024-08-24 12:07:51 +0530

    Updating codes till 24/08/24

---
+++ CS 201 (Algorithm)/Lecture Qns/Python/Test.py
@@ -0,0 +1,3 @@
+nums = [7,6,5,4,3,2,1]
+nums[0:7].sort()
+print(nums)
---
+++ Java/Lecture/JavaBasics.java
@@ -0,0 +1,12 @@
+public class JavaBasics {
+    public void Print()
+    {
+        int i = 5;
+        int j = 10;
+        System.out.println(5+10);
+        System.out.println(5+' '+10);
+
+        System.out.println(i+j);
+        System.out.println(i+' '+j);
+    }
+}
